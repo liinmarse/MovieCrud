e50e8515177ca24c1db751a4bc915ca91a50a8ca
liinmarse <liinmarse@gmail.com>
2023-10-18 10:11:46 -0500

    funcion createcategory que no funciona

--- ProjectMovie/Frontend/Index.js
+++ ProjectMovie/Frontend/Index.js
@@ -7,7 +7,7 @@ const CategoryURI = 'Category/';
 
 
 const getAllCategoryAsync = async (event) => {
-    console.log(event);
+    //console.log(event);
     event.preventDefault();
     const result = await fetch(baseURL+CategoryURI);
     const data = await result.json();
@@ -22,6 +22,20 @@ const getAllCategoryAsync = async (event) => {
 }
 
 getAllBtn.addEventListener('click', getAllCategoryAsync);
+
+
+const NameCategory = document.getElementById('NameCategory');
+const Create = document.getElementById('Create-btn');
+
+const CreateCategoryAsync = async (event) => {
+    event.preventDefault();
+    const Name = NameCategory.value;
+   
+    await fetch( `https://localhost:7285/Category?name=${Name}`,
+    {
+        method:'POST'});
+    }
+Create.addEventListener('submit', CreateCategoryAsync);
 
 
 
